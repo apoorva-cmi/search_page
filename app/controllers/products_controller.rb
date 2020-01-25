9ccd8e3c63350836a65776911ba7a2a3e9f019fd
Apoorva Jain <apoorva.jain3293@gmail.com>
2020-01-25 22:02:53 +0530

    Added Datatables

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,11 @@
+require 'product_datatable'
+
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    respond_to do |format|
+      format.html
+      format.json { render json: ProductsDatatable.new(view_context) }
+    end
   end
 end
---
+++ app/datatables/product_datatable.rb
@@ -0,0 +1,51 @@
+class ProductsDatatable < ApplicationDatatable
+    # delegate :edit_product_path, to: :@view
+  
+    private
+  
+    def data
+      products.map do |product|
+        [].tap do |column|
+          column << product.title
+          column << product.description
+          column << product.country
+          column << product.tags
+          column << product.price
+  
+        #   links = []
+        #   links << link_to('Show', user)
+        #   links << link_to('Edit', edit_product_path(user))
+        #   links << link_to('Destroy', user, method: :delete, data: { confirm: 'Are you sure?' })
+        #   column << links.join(' | ')
+        end
+      end
+    end
+  
+    def count
+      Product.count
+    end
+  
+    def total_entries
+      return 0 if products.blank?
+      products.total_count
+    end
+  
+    def products
+      @products ||= fetch_products
+    end
+  
+    def fetch_products
+      search_string = []
+      columns.each do |term|
+        search_string << "#{term} like :search"
+      end
+  
+      products = Product.order("#{sort_column} #{sort_direction}")
+      products = products.page(page).per(per_page)
+      products = products.where(search_string.join(' or '), search: "%#{params[:search][:value]}%")
+    end
+  
+    def columns
+      %w(title description country tags price)
+    end
+  end
